4bdf9af6d790b8640c074d91f6cfbfe8009dec08
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-08-26 22:30:41 +0530

    final app.py code

--- app.py
+++ app.py
@@ -1,115 +1,271 @@
 import streamlit as st
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
-import io
-import pyminizip
+import streamlit.components.v1 as components
+from streamlit_option_menu import option_menu
+import re
 
-# Google Sheets API credentials
-#enter scope , creds , client , sheet
+st.set_page_config(layout="wide", page_title="NeuroWell", page_icon="🧠")
 
+# Reducing whitespace on the top of the page
+st.markdown("""
+<style>
+.block-container {
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-top: 1rem;
+}
+</style>
+""", unsafe_allow_html=True)
 
-# Function to get all data from Google Sheets
-def get_all_data():
-    records = sheet.get_all_records()
-    df = pd.DataFrame(records)
-    return df
+# Set up the session state
+if 'user_info' not in st.session_state:
+    st.session_state.user_info = None
 
-# Function to add a new patient
-def add_new_patient(name, age, gender):
-    sheet.append_row([name, age, gender, "", "", "", ""])
+if 'user_type' not in st.session_state:
+    st.session_state.user_type = None
 
-# Function to get patient data by name
-def get_patient_data(name):
-    records = get_all_data()
-    return records[records['name'] == name]
+@st.cache_data
+def is_valid_email(email):
+    pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    return re.match(pattern, email) is not None
 
-def app():
+@st.cache_data
+def get_slideshow_html():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+    * { box-sizing: border-box; margin: 0; padding: 0; }
+    body { font-family: Verdana, sans-serif; }
+    .mySlides { display: none; }
+    img { vertical-align: middle; width: 100%; margin: 0; padding: 0; }
+    .slideshow-container { max-width: 400px; max-height: 400px; position: 100%; margin: 0; }
+    .numbertext { color: #f2f2f2; font-size: 12px; padding: 8px 12px; position: absolute; top: 0; }
+    .fade { animation-name: fade; animation-duration: 1.9s; }
+    @keyframes fade { from {opacity: .4} to {opacity: 1} }
+    </style>
+    </head>
+    <body>
+    <div class="slideshow-container">
+      <div class="mySlides fade"><div class="numbertext">1 / 4</div><img src="https://www.hiranandanihospital.org/public/uploads/blogs/1691067725.jpg"></div>
+      <div class="mySlides fade"><div class="numbertext">2 / 4</div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvCUh1GDnQgqs_1DZ0PbiconH4UKIjl5EOiQ&s"></div>
+      <div class="mySlides fade"><div class="numbertext">3 / 4</div><img src="https://risehealthgroup.com.au/wp-content/uploads/2022/03/nerve-pain.jpg"></div>
+      <div class="mySlides fade"><div class="numbertext">4 / 4</div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0Y5NNfX-nqjUAsSZS9__gutqQMxrGZDmTRw&s"></div>
+    </div>
+    <script>
+      let slideIndex = 0;
+      showSlides();
+      function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) { slides[i].style.display = "none"; }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1 }
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 2000);
+      }
+    </script>
+    </body>
+    </html>
+    """
+
+if st.session_state.user_info is None:
     gradient_text_html = """
-          <style>
-          .gradient-text {
-              font-weight: bold;
-              background: -webkit-linear-gradient(left, #07539e, #4fc3f7, #ffffff);
-              background: linear-gradient(to right, #07539e, #4fc3f7, #ffffff);
-              -webkit-background-clip: text;
-              -webkit-text-fill-color: transparent;
-              display: inline;
-              font-size: 2.9em;
-          }
-          </style>
-          <div class="gradient-text">Patient Management System</div>
-          """
-
+        <style>
+        .gradient-text {
+            font-weight: bold;
+            background: -webkit-linear-gradient(left, #07539e, #4fc3f7, #ffffff);
+            background: linear-gradient(to right, #07539e, #4fc3f7, #ffffff);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            display: inline;
+            font-size: 2.9em;
+        }
+        </style>
+        <div class="gradient-text">Welcome to Neurowell</div>
+        """
     st.markdown(gradient_text_html, unsafe_allow_html=True)
+    st.write(":orange[Your neuro-rehabilitation platform. Recovering from a stroke, brain injury, or other neurological conditions, we offer personalized plans, virtual therapy, and support tools]")
+
     st.image('divider.png')
+
+    st.write('Neurological disorders present immense challenges in rehabilitation due to the complexity of physical and mental hurdles, exacerbated by the lack of personalized and integrated solutions. Traditional approaches often fall short in addressing diverse patient needs, with limited accessibility and fragmented care hindering progress. Neurowell aims to revolutionize neuro-rehabilitation by offering personalized care plans, technology-driven accessibility, integration of rehabilitation efforts, and data-driven insights.')
     
-    col1, col2, col3 = st.columns([1, 0.15, 1])
-    
-    with col1:
-        option = st.selectbox("Select an option", ["New Patient", "Existing Patient"])
+    col4, col5, col7, col6, col8, col9, col10 = st.columns([0.03, 0.45, 0.03, 0.6, 0.03, 0.40, 0.02])
+    with col5:
+        st.write('')
+        st.write('')
+        components.html(get_slideshow_html(), height=300, width=400)
+        typing_animation = """
+            <h6 style="text-align: left;">
+            <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&Left=true&vLeft=true&width=500&height=47&lines=We Care++We Rehab🧠" alt="Typing Animation" />
+            </h6>
+            """
+        st.markdown(typing_animation, unsafe_allow_html=True)
 
-        if option == "New Patient":
-            st.subheader("Add New Patient")
-            name = st.text_input("Name")
-            age = st.text_input("Age")
-            gender = st.selectbox("Gender", ["Male", "Female", "Other"])
-            if st.button("Add Patient"):
-                add_new_patient(name, age, gender)
-                st.success(f"Patient {name} added successfully!")
+    with col6:
+        st.write(" ")
+        st.subheader(":orange[Our features]")
+        st.write(" - :blue[Comprehensive Stroke Assessment] - Upload CT scans and conduct thorough assessments using computer vision games, speech analysis, and memory matching games.")
+        st.write(" - :blue[Easy Patient Management] -  Seamlessly create and manage patient profiles with detailed medical histories and previous assessments.")
+        st.write(" - :blue[Advanced Diagnostic Insights]-  Receive detailed analysis of stroke type, affected brain areas, and patient performance with graphical representations")
+        st.write(" - :blue[Personalized Rehab Plans] - Get tailored rehabilitation plans ranging from independent exercises to motor impulse support and exoskeleton aid.")
 
-        elif option == "Existing Patient":
-            st.subheader("Retrieve Patient Information")
-            name = st.text_input("Enter Patient Name")
-            if st.button("Get Patient Info"):
-                patient_data = get_patient_data(name)
-                if not patient_data.empty:
-                    st.write(patient_data)
-                else:
-                    st.error("Patient not found!")
+    user_role = col9.selectbox('Are you a nurse or a patient?', ('Nurse', 'Patient'))
 
-    with col3:
-        st.subheader("All Patients Data")
-        all_data = get_all_data()
-        csv_data = all_data.to_csv(index=False)
+    do_you_have_an_account = col9.selectbox('Do you have an account?', options=('Yes', 'No', 'I forgot my password'))
 
-        # Save CSV data to a file
-        with open("patients_data.csv", "w") as file:
-            file.write(csv_data)
-        
-        # Create a password-protected ZIP file
-        zip_file = "encrypted_patients_data.zip"
-        password = "12345"
-        pyminizip.compress("patients_data.csv", None, zip_file, password, 5)
+    auth_form = col9.form(key='Authentication form', clear_on_submit=False)
+    email = auth_form.text_input(label='Email')
+    password = auth_form.text_input(label='Password', type='password') if do_you_have_an_account in {'Yes', 'No'} else auth_form.empty()
+    auth_notification = col9.empty()
 
-        # Read the ZIP file
-        with open(zip_file, "rb") as file:
-            zip_data = file.read()
+    if do_you_have_an_account == 'Yes' and auth_form.form_submit_button(label='Sign In', use_container_width=True, type='primary'):
+        if not email or not password:
+            auth_notification.warning("Both fields must be filled.")
+        elif not is_valid_email(email):
+            auth_notification.warning("Please enter a valid email address.")
+        else:
+            st.session_state.user_info = email
+            st.session_state.user_type = user_role
+            st.experimental_rerun()
 
-        # Allow users to download the password-protected ZIP file
-        st.download_button(
-            label="Download Encrypted ZIP",
-            data=zip_data,
-            file_name=zip_file,
-            mime="application/zip"
-        )
+    elif do_you_have_an_account == 'No' and auth_form.form_submit_button(label='Create Account', use_container_width=True, type='primary'):
+        if not email or not password:
+            auth_notification.warning("Both fields must be filled.")
+        elif not is_valid_email(email):
+            auth_notification.warning("Please enter a valid email address.")
+        else:
+            st.session_state.user_info = email
+            st.session_state.user_type = user_role
+            st.experimental_rerun()
+
+    elif do_you_have_an_account == 'I forgot my password' and auth_form.form_submit_button(label='Send Password Reset Email', use_container_width=True, type='primary'):
+        if not email:
+            auth_notification.warning("Email field must be filled.")
+        elif not is_valid_email(email):
+            auth_notification.warning("Please enter a valid email address.")
+        else:
+            auth_notification.success("Password reset link has been sent to your email.")
 
     st.image('divider.png')
 
+    col1, col2, col3 = st.columns([0.6, 1, 0.4])
+    with col2:
+        st.markdown('##### Project by team - :orange[BYTEBUDDIES] - Deekshith B, Sanjana W G')
 
+else:
+    if st.session_state.user_type == 'Nurse':
+        import home, physio, hand, game, result
 
+        class MultiApp:
+            def _init_(self):
+                self.app = []
 
-    st.image('divider.png')
+            def add_app(self, title, func):
+                self.app.append({"title": title, "function": func})   
 
-    st.subheader(':blue[Instructions to Nurse]')
-    st.write('1. Add new patients by selecting "New Patient" from the dropdown menu and if it is a old patient and wants all info please click existing user and retrieve his details')
-    st.write('2. There are 4 tests being done. Each has a audio guide choose your language and make sure you do not have any confusion before starting the process')
-    st.write('3. Monitor patient carefully and any adverse behaviour reach the medical team immediately')
-    st.write('4. Incase of any doubts contact us - bytebuddies@gmail.com')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
+            def run(self):
+                with st.sidebar:
+                    st.markdown("""
+                    <style>
+                        .gradient-text { margin-top: -20px; }
+                    </style>
+                    """, unsafe_allow_html=True)
+                    
+                    typing_animation = """
+                    <h3 style="text-align: left;">
+                    <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&Left=true&vLeft=true&width=500&height=70&lines=Neuro++Well🧠" alt="Typing Animation" />
+                    </h3>
+                    """
+                    st.markdown(typing_animation, unsafe_allow_html=True)
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    
+                    app = option_menu(
+                        menu_title='Sections',
+                        options=['Home', 'Physio🏋‍♂', 'hand🏋‍♂', 'game🐍', 'Result'],
+                        default_index=0,
+                    )
+                    
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    
+                    linkedin_url = "https://www.linkedin.com/in/deekshith2912/"
+                    linkedin_link = f"[ByteBuddies]({linkedin_url})"
+                    st.sidebar.header(f"Developed by {linkedin_link}")
+                    
+                if app == "Home":
+                    home.app()
+                elif app == "Physio🏋‍♂":
+                    physio.app()
+                elif app == "hand🏋‍♂":
+                    hand.app()
+                elif app == "game🐍":
+                    game.app()
+                elif app == "Result":
+                    result.app()
 
+        MultiApp().run()
 
-if __name__ == "_main_":
-    app()
+    elif st.session_state.user_type == 'Patient':
+         import homee, rehab, community
+         class MultiApp:
+            def _init_(self):
+                self.app = []
+
+            def add_app(self, title, func):
+                self.app.append({"title": title, "function": func})   
+
+            def run(self):
+                with st.sidebar:
+                    st.markdown("""
+                    <style>
+                        .gradient-text { margin-top: -20px; }
+                    </style>
+                    """, unsafe_allow_html=True)
+                    
+                    typing_animation = """
+                    <h3 style="text-align: left;">
+                    <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&Left=true&vLeft=true&width=500&height=70&lines=Neuro++Well🧠" alt="Typing Animation" />
+                    </h3>
+                    """
+                    st.markdown(typing_animation, unsafe_allow_html=True)
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    
+                    app = option_menu(
+                        menu_title='Sections',
+                        options=['Homee', 'Rehab🏋‍♂', 'Community'],
+                        default_index=0,
+                    )
+                    
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    st.sidebar.write("")
+                    
+                    linkedin_url = "https://www.linkedin.com/in/deekshith2912/"
+                    linkedin_link = f"[ByteBuddies]({linkedin_url})"
+                    st.sidebar.header(f"Developed by {linkedin_link}")
+                    
+                if app == "Homee":
+                    homee.app()
+                elif app == "Rehab🏋‍♂":
+                    rehab.app()
+                elif app == "Community":
+                    community.app()
+                
+
+    MultiApp().run()
